dc333f5ea40c4782f787d527bb3a7a0b7f2221b1
koreacat <kj9966@naver.com>
2024-02-09 22:39:48 +0900

    chore: 모바일 이벤트 추가

--- src/components/TransitionScene.tsx
+++ src/components/TransitionScene.tsx
@@ -11,6 +11,7 @@ interface TransitionWrapProps {
 const TransitionWrap = ({ children, onTransitionEnd, onTransitionStart }: TransitionWrapProps) => {
   const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
   const [index, setIndex] = useState<number>(0);
+  const [touchStartY, setTouchStartY] = useState<number>(0);
 
   const handleWheel = (e: React.WheelEvent) => {
     if (isTransitioning) return;
@@ -26,21 +27,56 @@ const TransitionWrap = ({ children, onTransitionEnd, onTransitionStart }: Transi
     }
   };
 
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setTouchStartY(e.touches[0].clientY);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    if (isTransitioning) return;
+
+    const deltaY = e.touches[0].clientY - touchStartY;
+    const threshold = 50; // Adjust this threshold as needed
+    if (Math.abs(deltaY) > threshold) {
+      const newIndex = index + (deltaY > 0 ? 1 : -1);
+      const length = Children.count(children);
+
+      if (newIndex >= 0 && newIndex < length) {
+        setIndex(newIndex);
+        setIsTransitioning(true);
+        onTransitionStart?.(index);
+      }
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setTouchStartY(0);
+  };
+
   const handleTransitionEnd = (index: number) => {
     setIsTransitioning(false);
     onTransitionEnd?.(index);
   };
 
   return (
-    <div className="w-full h-full overflow-hidden" onTransitionEnd={() => handleTransitionEnd(index)} onWheel={handleWheel}>
+    <div
+      className="w-full h-full overflow-hidden"
+      onWheel={handleWheel}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+      onTransitionEnd={() => handleTransitionEnd(index)}>
       <div
-        className="w-full scrollArea "
-        style={{ transition: isTransitioning ? 'transform .75s ease-in-out' : undefined, transform: `translate3d(0, -${index * 100}vh, 0)` }}>
+        className="w-full scrollArea"
+        style={{
+          transition: isTransitioning ? 'transform .75s ease-in-out' : undefined,
+          transform: `translate3d(0, -${index * 100}vh, 0)`
+        }}>
         {children}
       </div>
     </div>
-  )
-}
+  );
+};
+
 
 interface SceneProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: ReactNode;
